dfdfea80444fdfca96c0c2fdc52828b629729e90
AZhou <alexzhou.645@gmail.com>
2024-10-17 23:32:05 -0400

    Updated Main Page

--- LoginApp/app/src/main/java/com/example/loginapp/MainActivity.kt
+++ LoginApp/app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -1,51 +1,56 @@
+
+//import android.os.Bundle
+//import androidx.activity.ComponentActivity
+//import androidx.activity.compose.setContent
+//import androidx.activity.enableEdgeToEdge
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material3.Scaffold
+//import androidx.compose.material3.Text
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.tooling.preview.Preview
+//import com.example.loginapp.ui.theme.LoginAppTheme
+//
+//class MainActivity : ComponentActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//    }
+//}
+
 package com.example.loginapp
 
-
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.loginapp.ui.theme.LoginAppTheme
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import androidx.core.content.ContextCompat
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        enableEdgeToEdge()
-//        setContent {
-//            LoginAppTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    LoginAppTheme {
-//        Greeting("Android")
+
+        val btnSignIn = findViewById<Button>(R.id.btnSignInMain)
+        val btnRegister = findViewById<Button>(R.id.btnRegisterMain)
+
+        btnSignIn.setTextColor(ContextCompat.getColor(this, R.color.white))
+        btnSignIn.setBackgroundColor(ContextCompat.getColor(this, R.color.black))
+
+        btnRegister.setTextColor(ContextCompat.getColor(this, R.color.white))
+        btnRegister.setBackgroundColor(ContextCompat.getColor(this, R.color.purple_500))
+
+        btnSignIn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
-
 
